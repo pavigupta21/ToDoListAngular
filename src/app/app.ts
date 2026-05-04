80d14a0892ee90861beb3d1fa7329e099bacaf7d
Pavi Gupta <pavigupta21@gmail.com>
2026-05-04 14:34:36 +0530

    final commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,58 @@
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [FormsModule, CommonModule],
+//   templateUrl: './app.html'
+// })
+// export class App {
+//   task = '';
+//   tasks: string[] = [];
+
+//   addTask() {
+//     if (this.task.trim() !== '') {
+//       this.tasks.push(this.task);
+//       this.task = '';
+//       // this.tasks[0]="updated from code";
+//     }
+//   }
+
+//   deleteTask(i: number) {
+//     this.tasks.splice(i, 1);
+//   }
+
+//   editTask(i: number) {
+//     let updated = prompt('Edit task', this.tasks[i]);
+//     if (updated) this.tasks[i] = updated;
+//   }
+// }
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [FormsModule, CommonModule],
-  templateUrl: './app.html'
+  standalone:true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './app.html',
 })
 export class App {
-  task = '';
-  tasks: string[] = [];
+  newTask='';
+  tasks:String[]=[];
+  temp:String[]=[];
 
-  addTask() {
-    if (this.task.trim() !== '') {
-      this.tasks.push(this.task);
-      this.task = '';
-      // this.tasks[0]="updated from code";
-    }
+  add()
+  {
+    this.tasks.push(this.newTask);
+    this.temp.push(this.newTask);
+    this.newTask="";
   }
 
-  deleteTask(i: number) {
-    this.tasks.splice(i, 1);
-  }
-
-  editTask(i: number) {
-    let updated = prompt('Edit task', this.tasks[i]);
-    if (updated) this.tasks[i] = updated;
+  del(i:number)
+  {
+    this.tasks.splice(i,1);
   }
 }
